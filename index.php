33502227052e4e6df91e238b2ab177461830aa87
ChinhHM <chinh@HNchinhhm.southpacific.corp.microsoft.com>
2018-06-26 23:47:00 +0700

    connectivity test

--- index.php
+++ index.php
@@ -1,5 +1,15 @@
 <?php
-
-echo "Hello World!";
-
-?>
+2
+	$link = mysql_connect('vinpearl-mysqldbserver.mysql.database.azure.com', 'mysqldbuser@vinpearl-mysqldbserver', '1q2w3e4r!@#');
+3
+	if (!$link) {
+4
+	die('Could not connect: ' . mysql_error());
+5
+	}
+6
+	echo 'Connected successfully';
+7
+	mysql_close($link);
+8
+	?>
