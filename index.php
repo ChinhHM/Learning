1ae82e85b8d9cd42cff4c5e953d822b9fd113665
ChinhHM <chinh@HNchinhhm.southpacific.corp.microsoft.com>
2018-07-01 21:19:13 +0700

    4st

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 2
-	$host = 'chinhmysql01.mysql.database.azure.com';
+	/*$host = 'chinhmysql01.mysql.database.azure.com';
 	$username = 'mysqladmin@chinhmysql01';
 	$password = 'abc@12345';
 	//$db_name = 'your_database';
@@ -10,10 +10,10 @@
 	mysqli_real_connect($conn, $host, $username, $password, $db_name, 3306);
 	if (mysqli_connect_errno($conn)) {
 	die('Failed to connect to MySQL: '.mysqli_connect_error());
-	}
+	}*/
 6
-	echo 'Connected successfully';
+	echo 'Hello world';
 7
-	mysqli_close($conn);
+	//mysqli_close($conn);
 8
 	?>
